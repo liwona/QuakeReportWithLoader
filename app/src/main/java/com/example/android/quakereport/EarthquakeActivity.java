5f2fd793c8dd763124b873d3fde313dae06f09c5
liwona <liwona@users.noreply.github.com>
2018-05-19 18:05:26 +0200

    creating SettingActivity, setup the Menu (in xml) and in the EarthquakeActivity

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -27,6 +27,8 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -248,6 +250,25 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
 //            }
 //        }
 
+        @Override
+        // This method initialize the contents of the Activity's options main.
+        public boolean onCreateOptionsMenu(Menu menu) {
+            // Inflate the Options Menu we specified in XML
+            getMenuInflater().inflate(R.menu.main, menu);
+            return true;
+        }
+
+        @Override
+        // This method is called whenever an item in the options menu is selected.
+        public boolean onOptionsItemSelected(MenuItem item) {
+            int id = item.getItemId();
+            if (id == R.id.action_settings) {
+                Intent settingsIntent = new Intent(this, SettingsActivity.class);
+                startActivity(settingsIntent);
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
     }
 
 
